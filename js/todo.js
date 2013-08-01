301741aa564636f0c7d10c3d61fb5dfe76cd6b35
Equan Simmonds <simmonde@gmail.com>
2013-08-01 15:26:11 -0400

    Routes, Modules and config changes

---
+++ js/todo.js
@@ -0,0 +1,36 @@
+function TodoController($scope){
+
+	$scope.myTodoList = [
+		{text:'Learn javascript', done: true},
+		{text:'Learn Angualr', done: false},
+		{text:'Learn Knockout', done: false},
+	];
+
+	$scope.addTodo = function(){
+		$scope.myTodoList.push({text:$scope.todoAddText, done: false});
+
+		$scope.todoAddText = '';
+	};
+
+	$scope.getTotalTodos = function(){
+		return $scope.myTodoList.length;
+	};
+
+	$scope.getTotalTodos = function(){
+		return $scope.myTodoList.length;
+	};
+
+	$scope.remaining = function(){
+		return _.filter($scope.myTodoList, function(todo){
+			return !todo.done;
+		}).length;			
+	};
+
+	$scope.clearCompleted = function(){
+
+		$scope.myTodoList = _.filter($scope.myTodoList, function(todo){
+			return !todo.done;
+		});
+
+	};
+}
